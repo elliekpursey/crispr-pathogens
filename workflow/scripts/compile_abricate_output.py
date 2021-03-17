c5d5fff5c75fce3873de72d58f715f06a75cc78c
Ellie Pursey <61009410+elliekpursey@users.noreply.github.com>
2021-03-17 17:43:46 +0000

    Update compile_abricate_output.py

--- workflow/scripts/compile_abricate_output.py
+++ workflow/scripts/compile_abricate_output.py
@@ -9,6 +9,6 @@ Created on Thu May 21 11:17:59 2020
 import pandas as pd
 
 for input_file in list(snakemake.input):
-    results['id'] = input_file
     results = pd.read_csv(input_file, sep = '\t')
+    results['id'] = input_file
     results.to_csv(snakemake.output[0], mode='a', header=False)
