a2aee1d1c064f8454ae2401f9468f42ec969edd8
Ellie Pursey <61009410+elliekpursey@users.noreply.github.com>
2021-03-18 09:37:15 +0000

    Update compile_crisprcastyper_output.py

--- workflow/scripts/compile_crisprcastyper_output.py
+++ workflow/scripts/compile_crisprcastyper_output.py
@@ -8,6 +8,11 @@ Created on Thu May 21 11:17:59 2020
 
 import pandas as pd
 
+# create output files to ensure pipeline runs in case no CRISPR-Cas systems detected
+open(snakemake.output[0], 'a').close()
+open(snakemake.output[1], 'a').close()
+
+# compile CRISPR-Cas systems
 for input_file in list(snakemake.input):
     try:
         results = pd.read_csv(input_file + "/CRISPR_Cas.tab", sep = '\t')
@@ -17,6 +22,7 @@ for input_file in list(snakemake.input):
     except:
         print("no result for " + input_file)
 
+# compile CRISPR-Cas arrays
 for input_file in list(snakemake.input):
     try:
         results = pd.read_csv(input_file + "/crisprs_near_cas.tab", sep = '\t')
